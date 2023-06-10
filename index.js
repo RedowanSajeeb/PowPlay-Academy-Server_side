7f924cdbc8fd27cf35e0ce07e3ddd41a3cde3944
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-11 00:55:07 +0600

    first

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.use(express.json());
 
 
 
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.MGO_USER_PASS}@playacademy.z9xxo8h.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,10 +24,21 @@ const client = new MongoClient(uri, {
   },
 });
 
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const manageUsersCollection = client.db("PowerPlayUsers").collection("manageMdb");
+
+app.post("/users", async (req, res) => {
+    const user = req.body
+    const result = await manageUsersCollection.insertOne(user)
+     res.send(result);
+});
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -34,10 +46,11 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
+
 
 
 app.get('/' , async (req, res) => {
